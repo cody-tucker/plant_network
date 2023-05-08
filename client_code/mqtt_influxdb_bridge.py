445f6e57d4b1c07829a4c75c2aba8603b3fe39ad
ct_pi <cctucker5@gmail.com>
2023-05-07 18:07:53 -0700

    quick fix for two sensors now communicating with influx

--- client_code/mqtt_influxdb_bridge.py
+++ client_code/mqtt_influxdb_bridge.py
@@ -15,6 +15,9 @@ MQTT_PASSWORD = 'test1'
 MQTT_TOPIC = 'moisture'
 MQTT_CLIENT_ID = 'MQTT_InfluxDB_Bridge'
 
+top1 = 'indoor/palm/moisture'
+top2 = 'outdoor/veggies/moisture'
+
 influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
 
 channel_list = ['indoor/palm/moisture', 'outdoor/veggies/moisture']
@@ -31,7 +34,8 @@ class SensorData():
 def multi_channel_sub(client, channel_list):
 	for i in channel_list:
 		print(f'Subscribing to {i}..')
-		client.subscribe(i)
+		con = client.subscribe(i)
+		print(con)
 			
 	
 def on_connect(client, userdata, flags, rc):
@@ -39,10 +43,45 @@ def on_connect(client, userdata, flags, rc):
 		print("Connected with result code (rc)" +str(rc))
 
 		#client.subscribe(MQTT_TOPIC)
+		con1 = client.subscribe(top1)
+		print(con1)
+		con2 = client.subscribe(top2)
+		print(con2)
 		
 def on_message(client, userdata, msg):
 	"""The callback for when a PUBLISH message is received from the server
 	unless there is a specific on_message callback for that topic"""
+	print(f'The topic is: {str(msg.topic)} with msg: {str(msg.payload)}')
+	print(type(msg.payload))
+	payload = str(msg.payload)
+	print(str(msg.payload))
+	print(payload[2:-1])
+	topic = str(msg.topic)
+	
+	# Get the payload data we want. formatted as "Topic_666.00"
+	if payload[2:-1] == 'HELLO':
+		sensor_data = None
+	else:
+		data_payload = payload.split('_')[-1]
+		sensor_data = float(data_payload[0:-1])
+		
+		data_topic = topic.split('/')
+		# Now get the topic which will contain the sensor location (for now)
+		location_data = data_topic[1]
+		print(location_data)
+		measurement_data = 'soil_moisture'
+		
+		current_data = SensorData(location_data, measurement_data, sensor_data)
+		
+		
+	# if we have real data (not the test data 'HELLO') send it to influx db		
+	if sensor_data is not None:
+		send_sensor_data_to_influxdb(current_data)
+		
+def on_message_indoor(client, userdata, msg):
+	"""
+	for specific subscription... gonna take a different route rn...
+	"""
 	print(f'The topic is: {str(msg.topic)} with msg: {str(msg.payload)}')
 	print(type(msg.payload))
 	payload = str(msg.payload)
@@ -68,11 +107,6 @@ def on_message(client, userdata, msg):
 	# if we have real data (not the test data 'HELLO') send it to influx db		
 	if sensor_data is not None:
 		send_sensor_data_to_influxdb(current_data)
-		
-def message_callback_add(sub, callback):
-	"""
-	for specific subscription... gonna take a different route rn...
-	"""
 	
 	
 
@@ -103,8 +137,10 @@ def main(channel_list):
 	mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
 	mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
 	mqtt_client.on_connect = on_connect
-	multi_channel_sub(mqtt_client, channel_list)
+	#multi_channel_sub(mqtt_client, channel_list)
 	mqtt_client.on_message = on_message
+	#mqtt_client.message_callback_add("indoor/palm/moisture", on_message_indoor)
+	#mqtt_client.on_message_add("outdoor/veggies/moisture", on_message_outdoor)
 	
 	mqtt_client.connect(MQTT_ADDRESS, 1883)
 	mqtt_client.loop_forever()
